e7eab9866cfe0b56720e5217ba76673deb11fdc3
Md Samiul <samiuljust2018@gmail.com>
2025-04-16 19:14:13 +0600

    update

--- src/components/header/UserDropdown.jsx
+++ src/components/header/UserDropdown.jsx
@@ -51,7 +51,7 @@ export default function UserDropdown() {
         className="flex items-center text-gray-700 dropdown-toggle dark:text-gray-400"
       >
         <span className="mr-3 overflow-hidden rounded-full h-11 w-11">
-          <img src="/public/images/user/owner.jpg" alt="User" />
+          <img src="/images/user/owner.jpg" alt="User" />
         </span>
 
         <span className="block mr-1 font-medium text-theme-sm">{user_info?.name}</span>
--- src/layout/SidebarWidget.jsx
+++ src/layout/SidebarWidget.jsx
@@ -7,7 +7,7 @@ export default function SidebarWidget() {
       className={`flex items-center mx-auto mb-2 w-full max-w-60 rounded-2xl bg-gray-200 px-2 py-2 text-center dark:bg-white/[0.02]`}>
       
       <span className="mr-3 overflow-hidden rounded-full h-11 w-11">
-          <img src="/public/images/user/owner.jpg" alt="User" />
+          <img src="/images/user/owner.jpg" alt="User" />
         </span>
 
         <span className="block mr-1 font-medium text-theme-sm">{profile?.first_name}{profile?.last_name}</span>
