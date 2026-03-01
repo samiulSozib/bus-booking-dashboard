343952db7ffee0b8f51cae90af34419b653ef611
samiulSozib <samiulcse2018@gmail.com>
2026-03-01 09:57:08 +0600

    update

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,1 +1,4 @@
-export const base_url='https://api.milliekit.com/api/v1'
+// export const base_url='https://api.milliekit.com/api/v1'
+
+
+export const base_url='https://bus-booking-api-v2.milliekit.com/api/v1'
